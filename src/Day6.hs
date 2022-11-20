9b87cc963ea577f717a1222019a6d1a5d977d07f
ola berglund <olakjberglund@gmail.com>
2022-11-20 11:27:08 +0100

    cont 6b / sync between computers

--- src/Day6.hs
+++ src/Day6.hs
@@ -10,17 +10,23 @@ updateFish (Fish i) = if i < 1 then [Fish 6, Fish 8] else [Fish (i - 1)]
 solve :: String -> String
 solve = show . solveA
 
-nextday :: [Fish] -> [Fish]
-nextday = foldMap updateFish
-
 solveA :: String -> Int
 solveA s = length $ iterate nextday (fishes s) !! 80
 
 fishes :: String -> [Fish]
 fishes = map (Fish . read) . splitOn ","
 
+nextday :: [Fish] -> [Fish]
+nextday = foldMap updateFish
+
 -- above is too slow for B
 
--- Given a fish return the # of children after N days
+-- Given a fish return the # of direct children after N days
 children :: Fish -> Int -> Int
-children (Fish i) = undefined
+children (Fish i) d
+  | d <= i = 0
+  | otherwise = 1 + (d - i) `div` 6
+
+-- Given a fish return the # of total children after N days
+family :: Fish -> Int -> Int
+family = undefined
