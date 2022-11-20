ca482ee44a9b73625c551df0c1f9d39e23572a7f
ola berglund <olakjberglund@gmail.com>
2022-11-20 10:35:10 +0100

    solve 5a

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Day5
+import Day6
 
 main :: IO ()
 main = interact solve
---
+++ src/Day6.hs
@@ -0,0 +1,20 @@
+module Day6 where
+
+import Data.List.Split
+
+newtype Fish = Fish Int deriving Show
+
+updateFish :: Fish -> [Fish]
+updateFish (Fish i) = if i < 1 then [Fish 6, Fish 8] else [Fish (i - 1)]
+
+solve :: String -> String
+solve = show . solveA
+
+nextday :: [Fish] -> [Fish]
+nextday = foldMap updateFish
+
+solveA :: String -> Int
+solveA s = length $ iterate nextday (fishes s) !! 80
+
+fishes :: String -> [Fish]
+fishes =  map (Fish . read) . splitOn ","
