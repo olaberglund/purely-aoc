efccf098ff6a356924a26ef33ca5978d3a33e797
ola berglund <olakjberglund@gmail.com>
2022-11-20 11:04:30 +0100

    begin 6b

--- src/Day6.hs
+++ src/Day6.hs
@@ -2,7 +2,7 @@ module Day6 where
 
 import Data.List.Split
 
-newtype Fish = Fish Int deriving Show
+newtype Fish = Fish Int deriving (Show)
 
 updateFish :: Fish -> [Fish]
 updateFish (Fish i) = if i < 1 then [Fish 6, Fish 8] else [Fish (i - 1)]
@@ -17,4 +17,10 @@ solveA :: String -> Int
 solveA s = length $ iterate nextday (fishes s) !! 80
 
 fishes :: String -> [Fish]
-fishes =  map (Fish . read) . splitOn ","
+fishes = map (Fish . read) . splitOn ","
+
+-- above is too slow for B
+
+-- Given a fish return the # of children after N days
+children :: Fish -> Int -> Int
+children (Fish i) = undefined
