c71fef671bd6e7324368a7e165126f1b32916c16
ola berglund <olakjberglund@gmail.com>
2022-11-20 16:49:18 +0100

    finish day 6

--- src/Day6.hs
+++ src/Day6.hs
@@ -1,32 +1,36 @@
 module Day6 where
 
 import Data.List.Split
+import Data.MultiSet (MultiSet, size, fromList, concatMap)
 
-newtype Fish = Fish Int deriving (Show)
 
-updateFish :: Fish -> [Fish]
-updateFish (Fish i) = if i < 1 then [Fish 6, Fish 8] else [Fish (i - 1)]
+updateFish :: Int -> [Int]
+updateFish i = if i < 1 then [6, 8] else [i - 1]
 
 solve :: String -> String
-solve = show . solveA
+solve = show . solveB
 
 solveA :: String -> Int
 solveA s = length $ iterate nextday (fishes s) !! 80
 
-fishes :: String -> [Fish]
-fishes = map (Fish . read) . splitOn ","
+fishes :: String -> [Int]
+fishes = map read . splitOn ","
 
-nextday :: [Fish] -> [Fish]
+nextday :: [Int] -> [Int]
 nextday = foldMap updateFish
 
--- above is too slow for B
+-- below is not done by me
 
--- Given a fish return the # of direct children after N days
-children :: Fish -> Int -> Int
-children (Fish i) d
-  | d <= i = 0
-  | otherwise = 1 + (d - i) `div` 6
+simulate :: MultiSet Int -> MultiSet Int
+simulate = Data.MultiSet.concatMap (\i -> if i == 0 then [6, 8] else [i - 1])
 
--- Given a fish return the # of total children after N days
-family :: Fish -> Int -> Int
-family = undefined
+readInput :: String -> MultiSet Int
+readInput = fromList . map read . splitOn ","
+
+solveA' :: String -> String
+solveA' = show . size . (!! 80) . iterate simulate . readInput
+
+solveB :: String -> String
+solveB = show . size . (!! 256) . iterate simulate . readInput
+
+
