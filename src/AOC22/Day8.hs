ca28f9dc30d11e56bd407dedaed90225d98006f1
ola berglund <olakjberglund@gmail.com>
2022-12-08 22:21:14 +0100

    continue w/ 8b

--- src/AOC22/Day8.hs
+++ src/AOC22/Day8.hs
@@ -3,9 +3,9 @@
 module AOC22.Day8 where
 
 import Data.Char (digitToInt)
-import Data.Functor
 import Data.List
 import qualified Data.Map as Map
+import Utils (range)
 
 type Tree = Int
 
@@ -16,10 +16,13 @@ data Dir = U | D | L | R
 type Forest = Map.Map Pos Tree
 
 solve :: String -> String
-solve inp = show $ length $ filter (== True) $ map (`isVisible` forest) (coords (lines inp))
-  where
-    forest :: Forest
-    forest = mkForest $ lines inp
+solve = solveB . mkForest . lines
+
+solveB :: Forest -> String
+solveB forest = show $ maximum $ map (treeScore forest) (Map.keys forest)
+
+solveA :: Forest -> String
+solveA forest = show $ length $ filter (== True) $ map (`isVisible` forest) $ Map.keys forest
 
 coords :: [String] -> [Pos]
 coords inp = [(x, y) | y <- [0 .. length inp - 1], x <- [0 .. length (transpose inp) - 1]]
@@ -35,13 +38,12 @@ isVisible pos for = any (allShorter (Map.lookup pos for) . trees for pos) [U, D,
 trees :: Forest -> Pos -> Dir -> [Maybe Tree]
 trees for p dir = takeWhile (/= Nothing) $ map (`Map.lookup` for) (line p dir)
 
+treeScore :: Forest -> Pos -> Int
+treeScore for pos = product $ map (length . takeWhile (< Map.lookup pos for) . trees for pos) [U, D, L, R]
+
 line :: Pos -> Dir -> [Pos]
 line (x, y) = \case
-  U -> [(x, y') | y /= 0, y' <- [0 .. y - 1]]
+  U -> [(x, y') | y /= 0, y' <- range (y - 1) 0]
   D -> [(x, y') | y' <- [y + 1 ..]]
-  L -> [(x', y) | x /= 0, x' <- [0 .. x - 1]]
+  L -> [(x', y) | x /= 0, x' <- range (x - 1) 0]
   R -> [(x', y) | x' <- [x + 1 ..]]
-
-input = readFile "./inputs/22/input_8.txt"
-
-forest = input <&> (mkForest . lines)
